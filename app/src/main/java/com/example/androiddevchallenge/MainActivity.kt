82fb06101dd9f82bc16194b403aae1ee4ec322dc
Roman Tabi <roman.tabi@ahead-itec.com>
2021-03-01 18:37:18 +0100

    Imports

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -21,21 +21,32 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.LocationOn
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
@@ -47,7 +58,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.palette.graphics.Palette
 import com.example.androiddevchallenge.data.DataProvider
@@ -71,12 +81,12 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             MyTheme {
-                val destination by rememberSaveable { Navigator.currentDestination }
+                val destination = rememberSaveable { Navigator.currentDestination }
 
-                when (destination) {
+                when (destination.value) {
                     is Navigator.Destination.Home -> Home()
                     is Navigator.Destination.Detail ->
-                        ParrotDetail((destination as Navigator.Destination.Detail).parrotId)
+                        ParrotDetail((destination.value as Navigator.Destination.Detail).parrotId)
                 }
             }
         }
@@ -92,9 +102,9 @@ fun Home() {
             Header()
 
             val viewModel = viewModel(HomeViewModel::class.java)
-            val viewState by viewModel.state.collectAsState()
+            val viewState = viewModel.state.collectAsState()
 
-            ListParrots(viewState.parrots)
+            ListParrots(viewState.value.parrots)
         }
     }
 }
